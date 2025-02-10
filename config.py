15ce296a70fe89ffa785ac4cb7682adb478ad8e6
Metika13 <mewtikh111@gmail.com>
2025-02-10 21:35:16 +0330

    Create config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+# config.py
+BOT_TOKEN = "7765223935:AAE1PSF2JzymyDyWv_B4dqgH4hvQYjfTPaU"  # توکن ربات تلگرام خود را اینجا وارد کنید
+INSTAGRAM_USERNAME = "mtkh13o"  # نام کاربری اینستاگرام خود را اینجا وارد کنید
+INSTAGRAM_PASSWORD = "191275Mm"  # رمز عبور اینستاگرام خود را اینجا وارد کنید
